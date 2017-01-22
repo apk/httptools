7462f58bff8c38c11720ca23c1356864f3f85d8f
Andreas Krey <a.krey@aquilius.com>
2017-01-22 22:29:05 +0100

    add forwarder handler

---
+++ forwarder.go
@@ -0,0 +1,20 @@
+package httptools
+
+import (
+	"net/http"
+	"net/http/httputil"
+)
+
+func SSLForwarderHandleFunc(host string) func(w http.ResponseWriter, r *http.Request) {
+	revproxy := httputil.ReverseProxy{
+		Director: func(r *http.Request) {
+			r.URL.Scheme="https"
+			r.URL.Host=host
+			r.Host=host
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		revproxy.ServeHTTP(w, r)
+	}
+}
