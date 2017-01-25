c66759c113d69f276664e258be80608a9e787f71
Andreas Krey <g@apk.li>
2017-01-25 06:25:30 +0100

    add PiCam (hander to raspistill invocations)

---
+++ pic.go
@@ -0,0 +1,70 @@
+package httptools
+
+import (
+	"net/http"
+	"fmt"
+	"log"
+	"io/ioutil"
+	"os/exec"
+)
+
+type picreq struct {
+	ch chan []byte
+	size int
+}
+
+func to_s(x int) string {
+	return fmt.Sprintf("%d", x)
+}
+
+func picserve(ch chan picreq) {
+	for rq := range ch {
+		cmd := exec.Command(
+			"raspistill",
+			"-t", "5",
+			"-w", to_s (9 * 4 * rq.size),
+			"-h", to_s (9 * 3 * rq.size),
+			"-mm", "matrix",
+			"-o", "-")
+		out, err := cmd.Output()
+		if err != nil {
+			log.Print("Exec:", err)
+		}
+		rq.ch <- out
+	}
+}
+
+func PiCam(mux *http.ServeMux, path string) {
+
+	ch := make(chan picreq)
+
+	go picserve(ch)
+
+	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		_, err := ioutil.ReadAll(r.Body)
+		if err == nil {
+			rc := make(chan []byte)
+			ch <- picreq{ch: rc, size: 8}
+			s := <-rc
+			w.Write([]byte(s))
+		}
+	})
+
+	defhdlr := func (suf string, fac int) {
+		mux.HandleFunc(path + suf, func(w http.ResponseWriter, r *http.Request) {
+			_, err := ioutil.ReadAll(r.Body)
+			if err == nil {
+				rc := make(chan []byte)
+				ch <- picreq{ch: rc, size: fac}
+				s := <-rc
+				w.Write([]byte(s))
+			}
+		})
+	}
+
+	defhdlr ("/r", 4 * 9);
+	defhdlr ("/s", 2 * 9);
+	defhdlr ("/t", 15);
+	defhdlr ("/u", 9);
+	defhdlr ("/v", 5);
+}
