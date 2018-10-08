4157d9f82fe00adceba4f0f07fc8747840679bff
Andreas Krey <g@apk.li>
2018-10-08 21:13:21 +0200

    forwarder: add http forwarder

--- forwarder.go
+++ forwarder.go
@@ -18,3 +18,17 @@ func SSLForwarderHandleFunc(host string) func(w http.ResponseWriter, r *http.Req
 		revproxy.ServeHTTP(w, r)
 	}
 }
+
+func HttpForwarderHandleFunc(host string) func(w http.ResponseWriter, r *http.Request) {
+	revproxy := httputil.ReverseProxy{
+		Director: func(r *http.Request) {
+			r.URL.Scheme="http"
+			r.URL.Host=host
+			r.Host=host
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		revproxy.ServeHTTP(w, r)
+	}
+}
