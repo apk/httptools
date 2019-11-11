e5fcca8ba1ac4655d157a9cb9482c58a3b332548
Andreas Krey <g@apk.li>
2019-11-11 08:22:51 +0100

    pick up index and index.cgi before defaulting on a directory

--- gnord.go
+++ gnord.go
@@ -24,11 +24,39 @@ func GnordHandleFunc(opts *GnordOpts) func (w http.ResponseWriter, r *http.Reque
 		}
 
 		f, e := os.Lstat(file)
-		if e == nil && (f.Mode() & os.ModeSymlink != 0) {
-			s, e := os.Readlink(file)
-			if e == nil {
-				http.Redirect(w, r, s, http.StatusSeeOther)
-				return
+		if e == nil {
+			if (f.Mode() & os.ModeSymlink != 0) {
+				s, e := os.Readlink(file)
+				if e == nil {
+					http.Redirect(w, r, s, http.StatusSeeOther)
+					return
+				}
+			} else if (f.Mode() & os.ModeDir != 0) {
+				idxname := file + "/index"
+				cginame := idxname + ".cgi"
+
+				_, e = os.Stat(cginame)
+				if (e == nil) {
+					// Mostly common code with stuff below.
+					if opts.IpHeader != "" {
+						ff := r.Header.Get(opts.IpHeader)
+						if ff != "" {
+							r.RemoteAddr = ff
+						}
+					}
+					h := cgi.Handler{
+						Path: cginame,
+						Root: opts.Path,
+					}
+					h.ServeHTTP(w, r)
+					return
+				}
+
+				_, e = os.Stat(idxname)
+				if (e == nil) {
+					http.ServeFile(w, r, idxname)
+					return
+				}
 			}
 		}
 
@@ -50,6 +78,8 @@ func GnordHandleFunc(opts *GnordOpts) func (w http.ResponseWriter, r *http.Reque
 				return
 			}
 		}
+
+		// Serve by default handler.
 		http.ServeFile(w, r, file)
 	}
 }
